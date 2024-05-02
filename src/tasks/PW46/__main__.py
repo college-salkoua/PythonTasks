55b15b702632bd54f7a3484bd16390d6d2795ea9
salko-ua <chips69jamil69game@gmail.com>
2024-05-03 01:31:41 +0300

    add PW46 tasks

--- src/tasks/PW46/__main__.py
+++ src/tasks/PW46/__main__.py
@@ -1,6 +1,36 @@
 import os
 from src.open_tasks import open_it
+import tkinter as tk
 
+
+def first_task():
+    def draw():
+        canvas.delete('all')
+        try:
+            x = int(entry.get())
+            y = x
+            for i in range(10):
+                canvas.create_rectangle(0, 0, x, y, outline='purple')
+                x += x
+                y += y
+        except:
+            pass
+
+    root = tk.Tk()
+    root.geometry('300x200')
+    canvas = tk.Canvas(root, width=300, height=200)
+    canvas.pack()
+
+    label = tk.Label(root, text="Введіть розмір квадрату:")
+    label.pack()
+
+    entry = tk.Entry(root)
+    entry.pack()
+
+    button = tk.Button(root, text="Намалювати", command=draw)
+    button.pack()
+
+    root.mainloop()
 
 
 try:
