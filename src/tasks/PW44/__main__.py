be33286db885bae6d42ee91419d84e471009446c
salko-ua <chips69jamil69game@gmail.com>
2024-05-03 01:31:30 +0300

    add PW44 tasks

--- src/tasks/PW44/__main__.py
+++ src/tasks/PW44/__main__.py
@@ -1,6 +1,34 @@
 import os
 from src.open_tasks import open_it
+import tkinter as tk
 
+
+def first_task():
+    root = tk.Tk()
+    root.geometry('300x200')
+    canvas = tk.Canvas(root, width=300, height=200)
+    canvas.pack()
+    x = 0
+    y = 0
+    for i in range(10):
+        canvas.create_oval(x, y, x + 30, y + 30, outline='purple')
+        x += 30
+        y += 30
+    root.mainloop()
+
+
+def second_task():
+    root = tk.Tk()
+    root.geometry('300x200')
+    canvas = tk.Canvas(root, width=300, height=200)
+    canvas.pack()
+    x = 0
+    y = 0
+    for i in range(10):
+        canvas.create_text(x + 15, y + 15, text="Python", fill='purple')
+        x += 30
+        y += 30
+    root.mainloop()
 
 
 try:
