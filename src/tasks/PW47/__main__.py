dac523771746b9a2d33d690dafc72f0d43c6ca25
salko-ua <chips69jamil69game@gmail.com>
2024-05-03 01:31:54 +0300

    add PW47 tasks

--- src/tasks/PW47/__main__.py
+++ src/tasks/PW47/__main__.py
@@ -1,6 +1,18 @@
 import os
 from src.open_tasks import open_it
+import tkinter as tk
 
+
+def first_task():
+    import random
+    root = tk.Tk()
+    root.geometry('250x250')
+    canvas = tk.Canvas(root, width=1000, height=1000)
+    canvas.pack()
+    faces = [tk.PhotoImage(file=f"src/tasks/PW47/face{i}.png") for i in range(9)]
+    for i in range(9):
+        canvas.create_image(random.randint(0, 1000), random.randint(0, 1000), image=faces[i])
+    root.mainloop()
 
 
 try:
